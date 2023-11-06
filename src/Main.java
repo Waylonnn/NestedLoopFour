b4e48bb687e0132f3874580341f0c82dd29cb0d5
Waylon <weese.619547@swl.k12.oh.us>
2023-11-06 09:45:43 -0500

    Iniital

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        for(int i = 0; i < 10; i++){
+            System.out.print("**");
+            if(i >= 0 && i <= 3)
+                System.out.print("           **");
+            if(i == 4)
+                System.out.print("    **     **");
+            if(i == 5)
+                System.out.print("   ****    **");
+            if(i == 6)
+                System.out.print("  **  **   **");
+            if(i == 7)
+                System.out.print(" **     ** **");
+            if(i == 8)
+                System.out.print("**       ****");
+            if(i == 9)
+                System.out.print("           **");
+            System.out.println(" ");
+        }
+
+
+            }
+        }
